26eaacd9e8489fcaf6f5233980ab2d5ce041d2b0
Rahul Srivastava <srivasrk@mail.uc.edu>
2018-04-08 18:04:00 -0400

    start game button

--- app.js
+++ app.js
@@ -4,5 +4,12 @@
          playerHealth: 100,
          monsterHealth: 100,
          gameIsRunning: false
+     },
+     methods: {
+         startGame: function() {
+            this.gameIsRunning = true;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;             
+         }
      }
  });
